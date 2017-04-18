66f8478c06ddfd8da1e629725e8c62bd889b59c2
Franco Valencia <franco.avalencia@gmail.com>
2017-04-18 12:08:00 -0500

    Finished parsing methods

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import re, copy
 class Perceptron:
     def train(self):
         lines = Perceptron.process_input()
-        print (lines)
+        d = int(lines[0])
+        m = int(lines[1])
+        n = int(lines[2])
+        training_examples = Perceptron.parse_training_examples(lines, m)
+        test_examples = Perceptron.parse_test_examples(lines, m)
+
 
     def process_input():
         lines = []
@@ -15,6 +20,24 @@ class Perceptron:
         lines = list(map(lambda x: x.strip('').rstrip('\n').rstrip('\r').replace(' ', ''), lines))
 
         return lines
+
+    def parse_training_examples(lines, m):
+        training_examples = []
+        for index, line in enumerate(lines):
+            if index > 2 and index < 3 + m:
+                training_examples.append(line)
+
+        return training_examples
+
+
+    def parse_test_examples(lines, m):
+        test_examples = []
+        for index, line in enumerate(lines):
+            if index > 2 + m:
+                test_examples.append(line)
+
+        return test_examples
+
 
 
 def main():
